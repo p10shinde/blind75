c53b5d9bacf3b001bdff6aa895b0d0b64e817220
Pankaj Shinde <p10shinde@gmail.com>
2022-05-22 12:14:10 -0400

    Added complexity

--- 9Leetcode15.py
+++ 9Leetcode15.py
@@ -3,6 +3,10 @@
 # Given an array nums of n integers, are there elements a,b,c in nums such that
 # a + b + c = 0? Find all unique triplets in the array which gives the sum of zero
 
+# Time: O(nlog n) + O(n2) => O(n2)
+# Space: O(1) or O(n)
+
+
 nums = [-1, 0, 1, 2, -1, -4]
 res = []
 
