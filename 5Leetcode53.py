b881d6ae26037ba927ed3bc289a3b636c33fc3e8
Pankaj Shinde <p10shinde@gmail.com>
2022-05-21 23:39:34 -0400

    removed junk

--- 5Leetcode53.py
+++ 5Leetcode53.py
@@ -3,20 +3,9 @@
 # has the largest sum and return its sum.
 
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-# maxSum = float('-inf')
-# maxSumArr = []
-# for i in range(len(nums)):
-#     for j in range(i + 1, len(nums)):
-#         currsum = sum(nums[i:j+1])
-#         if currsum > maxSum:
-#             maxSum = currsum
-#             maxSumArr = nums[i:j+1]
-# print(maxSum)
-# print(maxSumArr)
-
-
 # Linear
-
+# Time: O(n)
+# Space: O(1)
 maxSub = nums[0]
 currsum = 0
 
