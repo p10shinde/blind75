7cfb951538b49e107bad7f692b055b99ded659c9
Pankaj Shinde <p10shinde@gmail.com>
2022-05-22 11:57:23 -0400

    problem 9

---
+++ 9Leetcode15.py
@@ -0,0 +1,28 @@
+# 3Sum
+
+# Given an array nums of n integers, are there elements a,b,c in nums such that
+# a + b + c = 0? Find all unique triplets in the array which gives the sum of zero
+
+nums = [-1, 0, 1, 2, -1, -4]
+res = []
+
+nums.sort()
+
+for i, value in enumerate(nums):
+    if i > 0 and value == nums[i-1]:
+        continue
+
+    l, r = i + 1, len(nums) - 1
+    while l < r:
+        threeSum = value + nums[l] + nums[r]
+
+        if threeSum > 0:
+            r -= 1
+        elif threeSum < 0:
+            l += 1
+        else:
+            res.append([value, nums[l], nums[r]])
+            l += 1
+            while nums[l] == nums[l - 1] and l < r:
+                l += 1
+print(res)
